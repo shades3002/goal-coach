10b18632dbecb5df8eec78be3937856445dfb6d1
shades3002 <shades3002@gmail.com>
2017-07-22 12:07:45 -0400

    Setting Up the Index

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SingIn extends Component {
+    render() {
+        return (
+            <div>SingIn</div>
+        )
+    }
+}
+
+export default SingIn;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import SingIn from './components/SingIn';
+import SignUp from './components/SingUp';
+
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+)
