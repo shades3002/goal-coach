d9744b210388ad400f1fbad03d922c8fa6c96457
shades3002 <shades3002@gmail.com>
2017-07-22 12:10:50 -0400

    fix index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import SingIn from './components/SingIn';
-import SignUp from './components/SingUp';
+import SingIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 
 ReactDOM.render(
