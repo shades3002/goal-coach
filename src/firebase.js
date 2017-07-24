f66d308e44607362a263f6fd3ec2cc5aea8b0487
shades3002 <shades3002@gmail.com>
2017-07-24 11:47:32 -0400

    Adding Firebase to the Application

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from  'firebase';
+
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+
+export const firebaseApp = firebase.initializeApp(config);
