4400bc6be3e6bee1c19b13a1ff073a34bb34711f
shades3002 <shades3002@gmail.com>
2017-07-24 12:18:55 -0400

    Setting Up Sign Up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SingUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        }
+    }
+
+    SingUp() {
+        console.log('this.state', this.state);
+        const { email, password } = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+                this.setState({
+                    error
+                })
+            })
+    }
+
     render() {
         return (
-            <div>SingUp</div>
+            <div className="form-inline">
+                <h2>Sign Up</h2>
+                <div className="form-group" style={{margin: '5%'}}>
+                    <input 
+                        className="form-control"
+                        type="text"
+                        style={{marginRight: '5px'}}
+                        placeholder="email"
+                        onChange={event => this.setState({email: event.target.value})}
+                    />
+                    <input 
+                        className="form-control"
+                        type="text"
+                        style={{marginRight: '5px'}}
+                        placeholder="password"
+                        onChange={event => this.setState({password: event.target.value})}
+                    />
+                    <button 
+                        className="btn btn-primary"
+                        type="button"
+                        onClick={() => this.SingUp()}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+            </div>
         )
     }
 }
