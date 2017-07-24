ceac8bd2754ef7d6f745591856e11b259d632756
shades3002 <shades3002@gmail.com>
2017-07-24 11:25:03 -0400

    Navigating with React-Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
-import SingIn from './components/SignIn';
+import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Router path="/" history={browserHistory}>
+        <Route path="/app" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+    </Router>, document.getElementById('root')
 )
