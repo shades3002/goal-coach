98b317caac90446439897d63c83de2c3845c3116
shades3002 <shades3002@gmail.com>
2017-07-24 15:23:03 -0400

    Combining our Reducers

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { goalRef } from '../firebase';
+import { setGoals } from '../actions';
 
 
 class GoalList extends Component {
@@ -11,14 +13,23 @@ class GoalList extends Component {
                 goals.push({ email, title });
             })
             console.log('goals', goals);
+            this.props.setGoals(goals);
         })
     }
 
     render() {
+        console.log('this.props.goals',this.props.goals);
         return(
             <div>Goal List</div>
         )
     }
 }
 
-export default GoalList;
+function mapStateToProps(state) {
+    const { goals } = state;
+    return {
+        goals
+    }
+}
+
+export default connect(mapStateToProps, { setGoals })(GoalList);
