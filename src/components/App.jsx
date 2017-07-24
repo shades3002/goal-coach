ad4d3e40a836d4d15e850eeeb7a68af67588e78d
shades3002 <shades3002@gmail.com>
2017-07-24 12:56:51 -0400

    Adding Redux to GoalCoach

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { firebaseApp } from "../firebase";
 
 class App extends Component {
+    signOut() {
+        firebaseApp.auth().signOut();
+    }
+
     render() {
         return (
-            <div>App GoalCoach</div>
+            <div>App GoalCoach
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.signOut()}
+                >
+                    Sign signOut
+                </button>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ import SignUp from './components/SignUp';
 firebaseApp.auth().onAuthStateChanged(user => {
     if(user) {
         console.log('user has signed in or up', user);
+        browserHistory.push('/app');
     } else {
         console.log('user has signed out or still needs to sign in.')
+        browserHistory.replace('/signin');
     }
 })
 ReactDOM.render(
